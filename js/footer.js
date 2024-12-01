a273c0a85049e18fc823186a70e40579ebbbb397
madushandbh94 <161420392+madushandbh94@users.noreply.github.com>
2024-12-01 19:30:00 +0530

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+// footer.js
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("footer.html")
+      .then(response => {
+        if (!response.ok) throw new Error("Failed to load footer");
+        return response.text();
+      })
+      .then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data;
+      })
+      .catch(error => console.error("Error loading footer:", error));
+  });
+  
